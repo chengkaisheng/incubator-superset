fd3c7ed7da71cbc36c88eb5f2d079e9e4022af5b
chengkai.sheng <chengkai.sheng@zhejintech.com>
2020-05-15 11:30:19 +0800

    fix add slice translating bug#8749, translating: [add slice complete]

--- superset-frontend/src/addSlice/AddSliceContainer.jsx
+++ superset-frontend/src/addSlice/AddSliceContainer.jsx
@@ -104,11 +104,8 @@ export default class AddSliceContainer extends React.PureComponent {
                 {t(
                   'If the datasource you are looking for is not ' +
                     'available in the list, ' +
-                    'follow the instructions on the how to add it on the ',
+                    'follow the instructions on the how to add it.',
                 )}
-                <a href="https://superset.apache.org/tutorial.html">
-                  {t('Superset tutorial')}
-                </a>
               </p>
             </div>
             <br />
