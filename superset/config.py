7e9b65c2d2f86ec0de63ee23839e8083f0cf8387
chengkai.sheng <chengkai.sheng@zhejintech.com>
2020-06-07 23:46:42 +0800

    [translating]: dashboard releated lefovers

--- superset/config.py
+++ superset/config.py
@@ -176,7 +176,7 @@ PROXY_FIX_CONFIG = {"x_for": 1, "x_proto": 1, "x_host": 1, "x_port": 1, "x_prefi
 # GLOBALS FOR APP Builder
 # ------------------------------
 # Uncomment to setup Your App name
-APP_NAME = "Superset"
+APP_NAME = "Datains2"
 
 # Uncomment to setup an App icon
 APP_ICON = "/static/assets/images/superset-logo@2x.png"
